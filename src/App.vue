44e2b28f63fb56d43bd0d310cddda2a2f1c582fb
FRANCISCO HIGUERA <franciscohiguera@gmail.com>
2022-07-23 16:25:28 -0500

    feat: add user register and store vuex

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <nav>
     <div class="nav-wrapper teal">
       <a href="#" class="brand-logo">To-Do App</a>
+      <ul class="right hide-on-med-and-down">
+        <li v-if="$store.state.isAuthenticated"><a href="#" >User: {{$store.state.userName}}</a></li>
+        <li v-if="$store.state.isAuthenticated"><a href="#" @click="logUserOut">Logout</a></li>
+      </ul>
     </div>
   </nav>
   <div class="container white" style="margin-top: 30px">
@@ -11,6 +15,34 @@
 
 <script>
 export default {
-  name: "App"
+  beforeCreate() {
+    if (!this.$store.state.isAuthenticated) {
+      return this.$router.push("/login");
+    }
+  },
+  name: "App",
+  computed: {
+    logUserOut() {
+      this.axios.defaults.headers.common['Authorization'] = null;
+      this.$store.commit('setAuthenticated', false);
+      this.$store.commit('setUserName', null);
+      this.$store.commit('setUserEmail', null);
+      this.$store.commit('setIsAdmin', null);
+      this.$router.push("/login");
+    },
+
+  },
 };
 </script>
+
+<style scoped>
+.user-data {
+  position: fixed;
+  bottom: 0px;
+  left: 0px;
+  padding: 10px;
+  background-color: white;
+  border-radius: 5px;
+  z-index: 2;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'material-design-icons/iconfont/material-icons.css'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import router from './router'
+import store from './store/store'
 axios.defaults.baseURL = 'https://localhost:3000/api/v1';
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
@@ -13,4 +14,4 @@ var token = localStorage.getItem('token');
 if(token)
     axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
 
-createApp(App).use(router).use(VueAxios, axios).mount('#app')
+createApp(App).use(router).use(store).use(VueAxios, axios).mount('#app')
