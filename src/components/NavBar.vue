6181be9f696a944e151975de60c99f02d3a97fbf
FRANCISCO HIGUERA <franciscohiguera@gmail.com>
2022-07-23 12:49:56 -0500

    feat: add auth and login

---
+++ src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav>
+    <div class="nav-wrapper teal">
+      <ul class="right hide-on-med-and-down">
+        <li>Logout</li>
+      </ul>
+    </div>
+  </nav> 
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  computed: {
+    email(){
+      return localStorage.getItem('userName')
+    },
+    id(){
+      return localStorage.getItem('id')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .user-data
+  {
+    position:fixed;
+    bottom: 0px; left: 0px;
+    padding:10px;
+    background-color:white;
+    border-radius:5px;
+    z-index: 2;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="row">
+    <h1>Welcome</h1>
+    <hr>
+    </div>
+</template>
